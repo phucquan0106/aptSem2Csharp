fbb804f29439bb2367708a0089ee5c53675a25ab
phuc <phucquan0106@gmail.com>
2019-04-20 21:18:41 +0700

    Csharp Assignment2

---
+++ Assignment2/CsharpAssignment2/CsharpAssignment2/Product.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpAssignment2
+{
+    class Product
+    {
+        public int id;
+        public string name;
+        public int price;
+        public int qty;
+        public string image;
+        public string desc;
+        public List<string> gallery;
+
+        public Product()
+        {
+        }
+
+        public Product(int id, string name, int price, int qty, string image, string desc, List<string> gallery)
+        {
+            this.id = id;
+            this.name = name;
+            this.price = price;
+            this.qty = qty;
+            this.image = image;
+            this.desc = desc;
+            this.gallery = gallery;
+        }
+
+        public void getInfo()
+        {
+            Console.WriteLine("id: " + id);
+            Console.WriteLine("name: " + name);
+            Console.WriteLine("price: " + price);
+            Console.WriteLine("qty: " + qty);
+            Console.WriteLine("image: " + image);
+            Console.WriteLine("desc: " + desc);
+            Console.WriteLine("Gallery: ");
+            int i = 0;
+            foreach (string s in gallery)
+            {
+                i++;
+                Console.WriteLine(i+". "+s);
+            }
+            Console.WriteLine();
+        }
+
+        public void checkStock()
+        {
+            if (gallery.Count > 0)
+            {
+                Console.WriteLine("In Stock!");
+            }
+            else {
+                Console.WriteLine("Out of Stock!");
+            }
+        }
+
+        public void insertGallery(string s)
+        {
+            if (gallery.Count > 10)
+            {
+                Console.WriteLine("Full! Please delete items to add more!");
+                return;
+            }
+            this.gallery.Add(s);
+        }
+
+        public void deleteGallery()
+        {
+            Console.WriteLine("Gallery: ");
+            int i = 0;
+            foreach (string s in gallery)
+            {
+                i++;
+                Console.WriteLine(i + ". " + s);
+            }
+            Console.WriteLine("Please input No. of Gallery to delete:");
+            int temp = Convert.ToInt32(Console.ReadLine());
+            this.gallery.RemoveAt(temp - 1);
+            Console.WriteLine("Gallery after delete: ");
+            i = 0;
+            foreach (string s in gallery)
+            {
+                i++;
+                Console.WriteLine(i + ". " + s);
+            }
+        }
+    }
+}
---
+++ Assignment2/CsharpAssignment2/CsharpAssignment2/Program.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CsharpAssignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product = null;
+            string choice;
+            string exit;
+            int id;
+            string name;
+            int price;
+            int qty;
+            string image;
+            string desc;
+            List<string> gallery=new List<string>();
+            
+            while (true)
+            {
+                Console.WriteLine("=========MENU========");
+                Console.WriteLine("1. Create Product.");
+                Console.WriteLine("2. Display product info.");
+                Console.WriteLine("3. Check stock.");
+                Console.WriteLine("4. Insert gallery");
+                Console.WriteLine("5. Delete gallery");
+                Console.WriteLine("6. Exit");
+                Console.WriteLine("Your choice: ");
+                choice = Console.ReadLine().Trim();
+                switch (choice)
+                {
+                    case ("1")://================Create Product==============
+                        if (product != null)
+                        {
+                            Console.WriteLine("Product exits!");
+                            continue;
+                        }
+
+                        Console.WriteLine("Please input product details: ");
+                        Console.WriteLine("id: ");
+                        id = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("name: ");
+                        name = Console.ReadLine();
+                        Console.WriteLine("price: ");
+                        price = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("qty: ");
+                        qty = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("image: ");
+                        image = Console.ReadLine();
+                        Console.WriteLine("desc: ");
+                        desc = Console.ReadLine();
+                        Console.WriteLine("gallery: ");
+                        gallery.Add(Console.ReadLine());
+                        product = new Product(id, name, price, qty, image, desc, gallery);
+
+                        Console.WriteLine("Enter number 1 to go back Menu or any other to EXIT");
+                        exit = Console.ReadLine().Trim();
+                        if (exit.Equals("1")) 
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            return;
+                        }
+                    case ("2")://================Display product info==============
+                        Console.WriteLine("Product info.: ");
+                        product.getInfo();
+
+                        Console.WriteLine("Enter number 1 to go back Menu or any other to EXIT");
+                        exit = Console.ReadLine().Trim();
+                        if (exit.Equals("1"))
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            return;
+                        }
+                    case ("3")://================Check stock==============
+                        Console.WriteLine("Check stock: ");
+                        product.checkStock();
+
+                        Console.WriteLine("Enter number 1 to go back Menu or any other to EXIT");
+                        exit = Console.ReadLine().Trim();
+                        if (exit.Equals("1"))
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            return;
+                        }
+                    case ("4")://================Insert gallery==============
+                        Console.WriteLine("input gallery name to insert:");
+                        product.insertGallery(Console.ReadLine());
+
+                        Console.WriteLine("Enter number 1 to go back Menu or any other to EXIT");
+                        exit = Console.ReadLine().Trim();
+                        if (exit.Equals("1"))
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            return;
+                        }
+                    case ("5")://================Delete gallery==============
+                       
+                        product.deleteGallery();
+
+                        Console.WriteLine("Enter number 1 to go back Menu or any other to EXIT");
+                        exit = Console.ReadLine().Trim();
+                        if (exit.Equals("1"))
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            return;
+                        }
+                    case ("6")://===================Exit====================
+                        return;
+
+                    default://=============Wrong Choice==============
+                        Console.WriteLine("Wrong Choice!");
+                        Console.WriteLine("Enter number 1 to go back Menu or any other to EXIT");
+                        exit = Console.ReadLine().Trim();
+                        if (exit.Equals("1"))
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            return;
+                        }
+
+                }
+            }
+        }
+    }
+}
