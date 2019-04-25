644512c04cb87e861dd0708cb4c9864345ce1bc5
phuc <phucquan0106@gmail.com>
2019-04-25 20:26:08 +0700

    demo override

---
+++ DemoOverride/DemoOverride/DemoOverride/Asian.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoOverride
+{
+    public class Asian : Human, HumanInterface, IHuman
+    {
+        public Asian()
+        {
+            this.id = 10;
+        }
+
+        public override void Run()
+        {
+
+        }
+
+        public new void Run(int number)
+        {
+
+        }
+
+        public void Jump()
+        {
+
+        }
+
+    }
+}
---
+++ DemoOverride/DemoOverride/DemoOverride/Human.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoOverride
+{
+    class Human
+    {
+        public int id;
+        public string name;
+        public int age;
+
+        public Human()
+        {
+        }
+
+        public virtual void Run()
+        {
+
+        }
+
+        public void Run(int number)
+        {
+
+        }
+
+        public void Run(string str)
+        {
+
+        }
+    }
+}
