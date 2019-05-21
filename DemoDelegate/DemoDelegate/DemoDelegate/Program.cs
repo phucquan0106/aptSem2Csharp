6edd6f72a0f8a549ac10c0874724ec5072589a24
phuc <phucquan0106@gmail.com>
2019-05-21 20:05:31 +0700

    Csharp generic demo

---
+++ DemoDelegate/DemoDelegate/DemoDelegate/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoDelegate
+{
+    //declare Delegate showString
+    public delegate void showString(string s);
+    public delegate void number(int a,int b);
+
+    public class EvenTest
+    {
+        public event showString Click;
+        public int age = 0;
+        public void showEvent()
+        {
+            if(Click != null)
+            {
+                Click("Click here...");
+            }
+        }
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                if(this.age != null && this.age != value)
+                {
+                    Click("Age was changed");
+                }
+                this.age = value;
+            }
+        }
+    }
+
+    class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            EvenTest ev = new EvenTest();
+            ev.Click += showStringProgram;
+            ev.Age = 20;
+            ev.Age = 22;
+
+            ev.showEvent();
+            
+            //map ss with showStringProgram
+            showString ss = new showString(showStringProgram);
+            showString s2 = new showString(showStringPro);
+            ss += s2;
+            ss("Hello ss");
+
+            ss.Invoke("Hello invoke");
+
+            number nPlus = new number(numberPlus);
+            number nMinus = new number(numberMinus);
+            number nMultiply = new number(numberMultiply);
+            number nDivide = new number(numberDivide);
+
+            nPlus(3, 2);
+            nPlus += nMultiply;
+            nPlus(5, 6);
+            nPlus += nMinus += nMultiply += nDivide;
+            nPlus(10, 5);
+
+            Console.ReadLine();
+        }
+        public static void showStringProgram(string x)
+        {
+            Console.WriteLine(x);
+        }
+
+        public static void showStringPro(string x)
+        {
+            Console.WriteLine("showStringPro " + x);
+        }
+
+        public static void numberPlus(int a, int b)
+        {
+            Console.WriteLine(a + b);
+        }
+
+        public static void numberMinus(int a, int b)
+        {
+            Console.WriteLine(a - b);
+        }
+
+        public static void numberMultiply(int a, int b)
+        {
+            Console.WriteLine(a * b);
+        }
+
+        public static void numberDivide(int a, int b)
+        {
+            Console.WriteLine(a / b);
+        }
+    }
+}
---
+++ DemoGeneric/DemoGeneric/DemoGeneric/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+namespace DemoGeneric
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Number<int> number = new Number<int>();
+            number.number_one = 20;
+            number.number_two = 122;
+            number.total();
+
+            number.genericMethod("param for generic method");
+
+            Number<float> number2 = new Number<float>();
+            number2.number_one = 12;
+            number2.number_two = (float)3.14;
+            number2.total();
+            // List<int> aa = new List<int>();
+            MyGeneric<int, string> xyz = new MyGeneric<int, string>();
+
+            Console.ReadLine();
+        }
+    }
+    public class MyGeneric<K, V>
+    {
+
+    }
+
+
+    public class Number<T>
+    {
+        public T number_one;
+        public T number_two;
+
+        public void total()
+        {
+            if (number_one is int)
+            {
+                int x = Convert.ToInt32(number_one);
+                int y = Convert.ToInt32(number_two);
+                Console.WriteLine(x + y);
+            }
+            else if (number_one is double || number_one is float)
+            {
+                double x = Convert.ToDouble(number_one);
+                double y = Convert.ToDouble(number_two);
+                Console.WriteLine(x + y);
+            }
+        }
+
+        public E genericMethod<E>(E val)
+        {
+            return val;
+        }
+
+        public B gemericMethod2<B>(B val1, B val2)
+        {
+            if (val1.Equals(val2))
+            {
+                return val1;
+            }
+            else
+            {
+                return val2;
+            }
+        }
+
+        public int genericMethod3<C>(C val1, C val2)
+        {
+            if (val1.Equals(val2))
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+    }
+
+}
