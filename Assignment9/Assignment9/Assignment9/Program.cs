5c2e16345baca4b634c5058b5c859b2f32da1240
phuc <phucquan0106@gmail.com>
2019-05-23 20:00:13 +0700

    Csharp Assignment 9

---
+++ Assignment9/Assignment9/Assignment9/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Assignment9
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Thread demNguoc = new Thread(RunThread);
+            demNguoc.Start();
+            Console.ReadLine();
+        }
+
+        public static void RunThread()
+        {
+            for (int j = 0; j<5; j++)
+            {
+                for (int i = 10; i > 0; i--)
+                {
+                    Console.WriteLine(i);
+                    Thread.Sleep(1000);
+                }
+                Console.WriteLine("Congratulation!");
+                Thread.Sleep(1000);
+                Console.Clear();
+            }
+            Console.WriteLine("THE END");
+        }
+
+        
+    }
+}
